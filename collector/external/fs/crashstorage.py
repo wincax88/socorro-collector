a24bfd800912521995bbe7f6b84b72360e2fb85a
Will Kahn-Greene <willkg@mozilla.com>
2016-07-05 10:59:48 -0400

    The culling: merge down to FSTemporaryStorage
    This squashes several class layers between FSRadixTreeStorage and
    FSTemporaryStorage that were there for legacy purposes to handle uses of
    filesystem crash storage that we don't do anymore.
    
    This is the first step in a several-step reduction on filesystem
    crashstorage classes.

--- collector/external/fs/crashstorage.py
+++ collector/external/fs/crashstorage.py
@@ -617,7 +617,7 @@ class FSLegacyDatedRadixTreeStorage(FSDatedRadixTreeStorage,
                 self.logger.critical("unknown file %s found", namedir)
 
 
-class FSTemporaryStorage(FSLegacyDatedRadixTreeStorage):
+class FSTemporaryStorage(FSRadixTreeStorage):
     """Temporary crash storage that uses only the day of the month as the root of
     the daily directories
 
@@ -626,6 +626,311 @@ class FSTemporaryStorage(FSLegacyDatedRadixTreeStorage):
 
     """
 
+    # From FSLegacyRadixTreeStroage
+    def _get_radixed_parent_directory(self, crash_id):
+        return os.sep.join(self._get_base(crash_id) +
+                           [self.config.name_branch_base] +
+                           self._get_radix(crash_id))
+
+
+    def remove(self, crash_id):
+        parent_dir = self._get_radixed_parent_directory(crash_id)
+        if not os.path.exists(parent_dir):
+            raise CrashIDNotFound
+
+        removal_candidates = [os.sep.join([parent_dir,
+                                           crash_id + '.json'])] + \
+                             list(self.get_raw_dumps_as_files(crash_id)
+                                  .values())
+
+        for cand in removal_candidates:
+            try:
+                os.unlink(cand)
+            except OSError:
+                self.config.logger.error("could not delete: %s", cand,
+                                         exc_info=True)
+
+
+    # From FSDatedRadixTreeStorage
+    required_config = Namespace()
+    required_config.add_option(
+        'date_branch_base',
+        doc='the directory base name to use for the dated radix tree storage',
+        default='date',
+        reference_value_from='resource.fs',
+    )
+    required_config.add_option(
+        'minute_slice_interval',
+        doc='how finely to slice minutes into slots, e.g. 4 means every 4 '
+            'seconds a new slot will be allocated',
+        default=4,
+        reference_value_from='resource.fs',
+    )
+
+    # This is just a constant for len(self._current_slot()).
+    SLOT_DEPTH = 2
+    DIR_DEPTH = 2
+
+    def _get_current_date(self):
+        date = utc_now()
+        return "%02d%02d%02d" % (date.year, date.month, date.day)
+
+    def _get_date_root_name(self, crash_id):
+        return 'date_root'
+
+    def _get_dump_file_name(self, crash_id, dump_name):
+        if dump_name == self.config.dump_field or dump_name is None:
+            return crash_id + self.config.dump_file_suffix
+        else:
+            return "%s.%s%s" % (crash_id,
+                                dump_name,
+                                self.config.dump_file_suffix)
+
+    def _get_dated_parent_directory(self, crash_id, slot):
+        return os.sep.join(self._get_base(crash_id) +
+                           [self.config.date_branch_base] + slot)
+
+    def _current_slot(self):
+        now = utc_now()
+        return ["%02d" % now.hour,
+                "%02d_%02d" % (now.minute,
+                               now.second //
+                                   self.config.minute_slice_interval)]
+
+    def _create_name_to_date_symlink(self, crash_id, slot):
+        """we traverse the path back up from date/slot... to make a link:
+           src:  "name"/radix.../crash_id (or "name"/radix... for legacy mode)
+           dest: "date"/slot.../crash_id"""
+        self._get_radixed_parent_directory(crash_id)
+
+        root = os.sep.join([os.path.pardir] * (self.SLOT_DEPTH + 1))
+        os.symlink(os.sep.join([root, self.config.name_branch_base] +
+                               self._get_radix(crash_id) +
+                               [crash_id]),
+                   os.sep.join([self._get_dated_parent_directory(crash_id,
+                                                                 slot),
+                                crash_id]))
+
+    def _create_date_to_name_symlink(self, crash_id, slot):
+        """the path is something like name/radix.../crash_id, so what we do is
+           add 2 to the directories to go up _dir_depth + len(radix).
+           we make a link:
+           src:  "date"/slot...
+           dest: "name"/radix.../crash_id/date_root_name"""
+        radixed_parent_dir = self._get_radixed_parent_directory(crash_id)
+
+        root = os.sep.join([os.path.pardir] *
+                           (len(self._get_radix(crash_id)) + self.DIR_DEPTH))
+        os.symlink(os.sep.join([root, self.config.date_branch_base] + slot),
+                   os.sep.join([radixed_parent_dir,
+                                self._get_date_root_name(crash_id)]))
+
+    def save_raw_crash(self, raw_crash, dumps, crash_id):
+        super(FSDatedRadixTreeStorage, self).save_raw_crash(raw_crash,
+                                                            dumps, crash_id)
+
+        slot = self._current_slot()
+        parent_dir = self._get_dated_parent_directory(crash_id, slot)
+
+        try:
+            os.makedirs(parent_dir)
+        except OSError:
+            # probably already created, ignore
+            pass
+            #self.logger.debug("could not make directory: %s" %
+                #parent_dir)
+
+        with using_umask(self.config.umask):
+            # Bug 971496 reversed the order of these calls so that the one that
+            # can fail will fail first and not leave an orphan symlink behind.
+            self._create_date_to_name_symlink(crash_id, slot)
+            self._create_name_to_date_symlink(crash_id, slot)
+
+    def remove(self, crash_id):
+        dated_path = os.path.realpath(
+            os.sep.join([self._get_radixed_parent_directory(crash_id),
+                         self._get_date_root_name(crash_id)]))
+
+        try:
+            # We can just unlink the symlink and later new_crashes will clean
+            # up for us.
+            os.unlink(os.sep.join([dated_path, crash_id]))
+        except OSError:
+            pass  # we might be trying to remove a visited crash and that's
+                  # okay
+
+        # Now we actually remove the crash.
+        super(FSDatedRadixTreeStorage, self).remove(crash_id)
+
+    def _visit_minute_slot(self, minute_slot_base):
+        for crash_id in os.listdir(minute_slot_base):
+            namedir = os.sep.join([minute_slot_base, crash_id])
+            st_result = os.lstat(namedir)
+
+            if stat.S_ISLNK(st_result.st_mode):
+                # This is a link, so we can dereference it to find
+                # crashes.
+                if os.path.isfile(
+                    os.sep.join([namedir,
+                                 crash_id +
+                                 self.config.json_file_suffix])):
+                    date_root_path = os.sep.join([
+                        namedir,
+                        self._get_date_root_name(crash_id)
+                    ])
+                    yield crash_id
+
+                    try:
+                        os.unlink(date_root_path)
+                    except OSError:
+                        self.logger.error("could not find a date root in "
+                                          "%s; is crash corrupt?",
+                                          namedir,
+                                          exc_info=True)
+
+                    os.unlink(namedir)
+
+    def new_crashes(self):
+        """
+        The ``new_crashes`` method returns a generator that visits all new
+        crashes like so:
+
+        * Traverse the date root to find all crashes.
+
+        * If we find a symlink in a slot, then we dereference the link and
+          check if the directory has crash data.
+
+        * if the directory does, then we remove the symlink in the slot,
+          clean up the parent directories if they're empty and then yield
+          the crash_id.
+        """
+        current_slot = self._current_slot()
+        current_date = self. _get_current_date()
+
+        dates = os.listdir(self.config.fs_root)
+        for date in dates:
+            dated_base = os.sep.join([self.config.fs_root, date,
+                                      self.config.date_branch_base])
+
+            try:
+                hour_slots = os.listdir(dated_base)
+            except OSError:
+                # it is okay that the date root doesn't exist - skip on to
+                # the next date
+                #self.logger.info("date root for %s doesn't exist" % date)
+                continue
+
+            for hour_slot in hour_slots:
+                skip_dir = False
+                hour_slot_base = os.sep.join([dated_base, hour_slot])
+                for minute_slot in os.listdir(hour_slot_base):
+                    minute_slot_base = os.sep.join([hour_slot_base,
+                                                    minute_slot])
+                    slot = [hour_slot, minute_slot]
+
+                    if slot >= current_slot and date >= current_date:
+                        # the slot is currently being used, we want to skip it
+                        # for now
+                        self.logger.info("not processing slot: %s/%s" %
+                                         tuple(slot))
+                        skip_dir = True
+                        continue
+
+                    for x in self._visit_minute_slot(minute_slot_base):
+                        yield x
+
+                    try:
+                        # We've finished processing the slot, so we can remove
+                        # it.
+                        os.rmdir(minute_slot_base)
+                    except OSError:
+                        self.logger.error("could not fully remove directory: "
+                                          "%s; are there more crashes in it?",
+                                          minute_slot_base,
+                                          exc_info=True)
+
+                if not skip_dir and hour_slot < current_slot[0]:
+                    try:
+                        # If the current slot is greater than the hour slot
+                        # we're processing, then we can conclude the directory
+                        # is safe to remove.
+                        os.rmdir(hour_slot_base)
+                    except OSError:
+                        self.logger.error("could not fully remove directory: "
+                                          "%s; are there more crashes in it?",
+                                          hour_slot_base,
+                                          exc_info=True)
+
+
+    # From FSLegacyDatedRadixTreeStorage
+    DIR_DEPTH = 1
+
+    def _get_date_root_name(self, crash_id):
+        return crash_id
+
+    def _create_name_to_date_symlink(self, crash_id, slot):
+        root = os.sep.join([os.path.pardir] * (self.SLOT_DEPTH + 1))
+        os.symlink(os.sep.join([root, self.config.name_branch_base] +
+                               self._get_radix(crash_id)),
+                   os.sep.join([self._get_dated_parent_directory(crash_id,
+                                                                 slot),
+                                crash_id]))
+
+    def _visit_minute_slot(self, minute_slot_base):
+        for crash_id_or_webhead in os.listdir(minute_slot_base):
+            namedir = os.sep.join([minute_slot_base, crash_id_or_webhead])
+            st_result = os.lstat(namedir)
+
+            if stat.S_ISLNK(st_result.st_mode):
+                crash_id = crash_id_or_webhead
+
+                # This is a link, so we can dereference it to find
+                # crashes.
+                if os.path.isfile(
+                    os.sep.join([namedir,
+                                 crash_id +
+                                 self.config.json_file_suffix])):
+                    date_root_path = os.sep.join([
+                        namedir,
+                        self._get_date_root_name(crash_id)
+                    ])
+
+                    yield crash_id
+
+                    try:
+                        os.unlink(date_root_path)
+                    except OSError:
+                        self.logger.error("could not find a date root in "
+                                          "%s; is crash corrupt?",
+                                          date_root_path,
+                                          exc_info=True)
+                # Bug 971496 - by outdenting this line one level we make sure
+                # that we can delete any orphan symlinks created by duplicate
+                # crash_ids in the file system
+                os.unlink(namedir)
+
+            elif stat.S_ISDIR(st_result.st_mode):
+                webhead_slot = crash_id_or_webhead
+                webhead_slot_base = os.sep.join([minute_slot_base,
+                                                 webhead_slot])
+
+                # This is actually a webhead slot, but we can visit it as if
+                # it was a minute slot.
+                for x in self._visit_minute_slot(webhead_slot_base):
+                    yield x
+
+                try:
+                    os.rmdir(webhead_slot_base)
+                except OSError:
+                    self.logger.error("could not fully remove directory: "
+                                      "%s; are there more crashes in it?",
+                                      webhead_slot_base,
+                                      exc_info=True)
+            else:
+                self.logger.critical("unknown file %s found", namedir)
+
+
+    # From FSTemporaryStorage
     def _get_current_date(self):
         date = utc_now()
         return "%02d" % date.day
